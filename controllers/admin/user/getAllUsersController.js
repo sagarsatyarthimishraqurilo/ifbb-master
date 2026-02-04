20065b1a3e2a24ef69e60b8f7af4b6006f634d75
sagarsatyarthimishraqurilo <sagarsatyarthimishraqurilo@gmail.com>
2026-02-04 15:34:33 +0530

    fixing getting user to admin sode

--- controllers/admin/user/getAllUsersController.js
+++ controllers/admin/user/getAllUsersController.js
@@ -1,7 +1,20 @@
 import User from '../../../models/userModel.js';
 const getAllUsersController = async (req, res) => {
   try {
-    const users = await User.find({});
+    const users = await User.find({}).populate({
+      path: 'purchasedCourses',
+      select: `
+          title 
+          description 
+          courseThumbnail 
+          price 
+          discountedPrice 
+          durationToComplete 
+          ratings 
+          isPublic
+          createdAt
+        `,
+    });
     return res.json(users);
   } catch (error) {
     console.error('Admin fetching Users Error', error);
