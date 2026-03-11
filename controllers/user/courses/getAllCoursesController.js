7800182e840cf868bb7d3686d76c60fbe7ace2cb
sagarsatyarthimishraqurilo <sagarsatyarthimishraqurilo@gmail.com>
2026-03-11 16:06:43 +0530

    update the get all cource apis on show un purchased id

--- controllers/user/courses/getAllCoursesController.js
+++ controllers/user/courses/getAllCoursesController.js
@@ -1,41 +1,38 @@
-import Course from '../../../models/courseModel.js';
-import User from '../../../models/userModel.js';
+import Course from "../../../models/courseModel.js";
+import User from "../../../models/userModel.js";
 
 export const getAllCoursesController = async (req, res) => {
   try {
-    let userId = null;
+
     let purchasedCourses = [];
 
-    // Handle optional auth: if middleware added req.user, use it
+    // if user logged in
     if (req.user && req.user.userId) {
-      userId = req.user.userId;
 
-      const user = await User.findById(userId).select('purchasedCourses');
-      if (user) {
-        purchasedCourses = user.purchasedCourses.map((id) => id.toString());
+      const user = await User.findById(req.user.userId).select("purchasedCourses");
+
+      if (user && user.purchasedCourses.length > 0) {
+        purchasedCourses = user.purchasedCourses;
       }
     }
 
-    const courses = await Course.find({ isPublic: true }).lean();
+    // filter courses which are NOT purchased
+    const courses = await Course.find({
+      isPublic: true,
+      _id: { $nin: purchasedCourses }
+    }).lean();
 
-    const modifiedCourses = courses.map((course) => {
-      const hasPurchased = purchasedCourses.includes(course._id.toString());
-
-      const modules = course.modules.map((module) => {
-        const { assetLink, ...rest } = module;
-        return hasPurchased ? module : rest;
-      });
-
-      return {
-        ...course,
-        ...(userId && { hasPurchased }),
-        modules,
-      };
+    return res.json({
+      success: true,
+      courses
     });
 
-    return res.json(modifiedCourses);
   } catch (error) {
-    console.error('Fetching Error Course:', error);
-    return res.status(500).json({ message: 'Could Not Fetch Courses' });
+    console.error("Fetching Error Course:", error);
+
+    return res.status(500).json({
+      success: false,
+      message: "Could Not Fetch Courses",
+    });
   }
 };
