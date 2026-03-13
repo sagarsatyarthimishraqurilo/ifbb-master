7fe80f9807e287eb8e31d53243da3112ccc2408c
sagarsatyarthimishraqurilo <sagarsatyarthimishraqurilo@gmail.com>
2026-03-13 15:50:52 +0530

    add proexy in index.js url

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-  import express from 'express';
-  import dotenv from 'dotenv';
-  import cors from 'cors';
-  import morgan from 'morgan';
-  import chalk from 'chalk';
-  import dbConnect from './utils/dbConnection.js';
-  import userAuthRoutes from './routes/user/userAuthRoutes.js';
-  import adminAuthRoutes from './routes/admin/adminAuthRoutes.js';
-  import userDataRoutes from './routes/user/userDataRoutes.js';
-  import adminCourseRoutes from './routes/admin/adminCourseRoutes.js';
-  import adminDataRoutes from './routes/admin/adminDataRoutes.js';
-  import paymentRoutes from './routes/payments/paymentRoutes.js';
-  import cookieParser from 'cookie-parser';
-  import userAuthMiddleware from './middleware/userAuthMiddleware.js';
-  import adminAuthMiddleware from './middleware/adminAuthMiddleware.js';
-  import adminNewsRoutes from "./routes/admin/adminNewsRoutes.js";
-  import newsRoutes from "./routes/common/newsRoutes.js";
-  import courseInquiryRoutes from "./routes/common/courseInquiryRoutes.js";
-  import adminInquiryRoutes from "./routes/admin/adminInquiryRoutes.js";
-  import adminCertificateRoutes from "./routes/admin/adminCertificateRoutes.js";
-  import certificateRoutes from "./routes/common/certificateRoutes.js";
-  import adminGalleryRoutes from "./routes/admin/galleryRoutes.js";
-  import commonGalleryRoutes from "./routes/common/galleryRoutes.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
+import chalk from 'chalk';
+import dbConnect from './utils/dbConnection.js';
+import userAuthRoutes from './routes/user/userAuthRoutes.js';
+import adminAuthRoutes from './routes/admin/adminAuthRoutes.js';
+import userDataRoutes from './routes/user/userDataRoutes.js';
+import adminCourseRoutes from './routes/admin/adminCourseRoutes.js';
+import adminDataRoutes from './routes/admin/adminDataRoutes.js';
+import paymentRoutes from './routes/payments/paymentRoutes.js';
+import cookieParser from 'cookie-parser';
+import userAuthMiddleware from './middleware/userAuthMiddleware.js';
+import adminAuthMiddleware from './middleware/adminAuthMiddleware.js';
+import adminNewsRoutes from "./routes/admin/adminNewsRoutes.js";
+import newsRoutes from "./routes/common/newsRoutes.js";
+import courseInquiryRoutes from "./routes/common/courseInquiryRoutes.js";
+import adminInquiryRoutes from "./routes/admin/adminInquiryRoutes.js";
+import adminCertificateRoutes from "./routes/admin/adminCertificateRoutes.js";
+import certificateRoutes from "./routes/common/certificateRoutes.js";
+import adminGalleryRoutes from "./routes/admin/galleryRoutes.js";
+import commonGalleryRoutes from "./routes/common/galleryRoutes.js";
 
 
-  dotenv.config();
-  const app = express();
+dotenv.config();
+const app = express();
+// ⭐ MUST USE Render PORT
+const PORT = process.env.PORT || 5003;
 
-  // ⭐ MUST USE Render PORT
-  const PORT = process.env.PORT || 5003;
-
-  // Middlewares
-  app.use(cookieParser());
+// Middlewares
+app.use(cookieParser());
+app.set("trust proxy", true);
 
 app.use(
   cors({
@@ -45,34 +45,34 @@ app.use(
 );
 
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
-  app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
 
-  // Routes
-  app.use('/api/user', userDataRoutes);
-  app.use('/api/user', userAuthRoutes);
-  app.use('/api/admin', adminAuthRoutes);
-  app.use('/api/admin',  adminCourseRoutes);
-  app.use('/api/admin',  adminDataRoutes);
-  app.use('/api/payments', paymentRoutes);
-  app.use("/api/admin", adminNewsRoutes); 
-  app.use("/api", newsRoutes);  
-  app.use("/api", courseInquiryRoutes);
-  app.use("/api/admin", adminInquiryRoutes);
-  app.use("/api/admin", adminCertificateRoutes);
-  app.use("/api", certificateRoutes);
-  app.use("/api/admin", adminGalleryRoutes);
-  app.use("/api", commonGalleryRoutes);
-  app.use("/uploads", express.static("uploads"));
+// Routes
+app.use('/api/user', userDataRoutes);
+app.use('/api/user', userAuthRoutes);
+app.use('/api/admin', adminAuthRoutes);
+app.use('/api/admin', adminCourseRoutes);
+app.use('/api/admin', adminDataRoutes);
+app.use('/api/payments', paymentRoutes);
+app.use("/api/admin", adminNewsRoutes);
+app.use("/api", newsRoutes);
+app.use("/api", courseInquiryRoutes);
+app.use("/api/admin", adminInquiryRoutes);
+app.use("/api/admin", adminCertificateRoutes);
+app.use("/api", certificateRoutes);
+app.use("/api/admin", adminGalleryRoutes);
+app.use("/api", commonGalleryRoutes);
+app.use("/uploads", express.static("uploads"));
 
-  app.get("/", (req, res) => {
-    res.send("IFBB Backend Running Successfully 🚀");
+app.get("/", (req, res) => {
+  res.send("IFBB Backend Running Successfully 🚀");
+});
+
+// ⭐ CONNECT DB + START SERVER
+dbConnect().then(() => {
+  app.listen(PORT, () => {
+    console.log(chalk.green(`🚀 Server started on PORT: ${PORT}`));
   });
-
-  // ⭐ CONNECT DB + START SERVER
-  dbConnect().then(() => {
-    app.listen(PORT, () => {
-      console.log(chalk.green(`🚀 Server started on PORT: ${PORT}`));
-    });
-  });
+});
